c7a6f549b98367cb86d6ba341c2143972fd29fce
Toroshhin <https://github.com/Toroshhin>
2019-12-10 21:26:30 +0300

    Modified function

--- Task2/main.js
+++ Task2/main.js
@@ -1,24 +1,15 @@
-window.onload = function () {
-  let oneArray = ['Миша', 'Кирилл', 'Василий',];
-  let twoArray = ['Кирилл', 'Кирилл', 'Василий',];
-
-  document.write('Первый массив: ' + oneArray + '<br>');
-  document.write('Второй массив: ' + twoArray + '<br><br>');
-
-  if (oneArray.length > twoArray.lenght) {
-    var maxLength = oneArray.length;
-  } else {
-    var maxLength = twoArray.length;
-  }
-
-  for (let i = 0; i < maxLength; i++) {
-
-    if (oneArray[i] == twoArray[i]) {
-      document.write('Элемент массива 1 под номером №' + (i + 1) + '[' + oneArray[i] + '] Равен элементу массива 2 под номером №' + (i + 1) + '[' + twoArray[i] + ']<br>');
-    } else {
-      document.write('Элемент массива 1 под номером №' + (i + 1) + '[' + oneArray[i] + '] Не равен элементу массива 2 под номером №' + (i + 1) + '[' + twoArray[i] + ']<br>');
+function isArrayEqual(oneArray, twoArray) {
+  let boolean = true;
+  if (Array.isArray(oneArray) === Array.isArray(twoArray) && oneArray.length === twoArray.length) {
+    for (let i = 0; i < oneArray.length; i++) {
+      if (oneArray[i] !== twoArray[i]) {
+        boolean = false;
+        break;
+      } else {
+        return boolean;
+      }
     }
+  } else {
+    return false;
   }
 }
-
-
