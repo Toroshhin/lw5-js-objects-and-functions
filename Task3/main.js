0bfa691d56d2baf96145812c1732bdc298059de8
Toroshhin <https://github.com/Toroshhin>
2019-12-14 14:15:21 +0300

    fixed bugs

--- Task3/main.js
+++ Task3/main.js
@@ -1,10 +1,20 @@
-window.onload = function () {
-  let oneArray = ['Миша', 'Борис', 'Инокентий',];
-  let twoArray = ['Андрей', 'Кирилл', oneArray,];
 
-  console.log(oneArray);
-  console.log(twoArray);
-
-  let newArray = twoArray.flat();
-  console.log(newArray);
+function flatArray(array) {
+  if (Array.isArray(array)) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+      if (!Array.isArray(array[i]) && typeof array[i] === 'number' && !Number.isNaN(array[i])) { //если элемент массива - чиcло
+        newArray.push(array[i]);
+      } else if (Array.isArray(array[i])) {
+        for (let k = 0; k < array[i].length; k++) {
+          if (typeof array[i][k] === 'number') {
+            newArray.push(array[i][k]);
+          }
+        }
+      }
+    }
+      return newArray; 
+  } else {
+    return false;
+  }
 }
